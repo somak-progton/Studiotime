24dddcdcffefeb5d3b8b6693373406c3ef16afed
somak@progton.com <somak-progton>
2019-10-09 17:44:23 +0530

    Added activestorage image uploading support for studio

--- app/models/studio.rb
+++ app/models/studio.rb
@@ -3,5 +3,6 @@ class Studio < ApplicationRecord
 	has_many :users, through: :bookings
 	has_many :reviews
 	has_many :users, through: :reviews
+	has_many_attached :images
 	belongs_to :studio_owner
 end
